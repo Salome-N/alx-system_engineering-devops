7f012bb591f1a11ccb0b48aab30a47d488c50c70
Salome-N <njengasalome87@gmail.com>
2024-02-25 13:38:04 -0800

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,3 +5,9 @@ file_line { 'Declare Identity File':
   line   => '    IdentityFile ~/.ssh/school',
   path   => '/etc/ssh/ssh_config',
 }
+
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  line   => '    PasswordAuthentication no',
+  path   => '/etc/ssh/ssh_config',
+}
