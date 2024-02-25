12118d3a00dacbaddc19870ea182fb3e1178c693
Salome-N <njengasalome87@gmail.com>
2024-02-25 13:29:18 -0800

    Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,7 @@
+# Puppet to make changes to our configuration file
+
+file_line { 'Declare Identity File':
+  ensure => 'present',
+  line   => '    IdentityFile ~/.ssh/school'
+  path   => '/etc/ssh/ssh_config',
+}
